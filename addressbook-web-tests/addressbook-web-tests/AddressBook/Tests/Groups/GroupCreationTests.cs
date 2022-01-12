2866b6515954f3e79e88ffe15f48e26c809e223a
Nzhi <lt.e.sempre.bene@gmail.com>
2022-01-12 21:02:33 +0300

    After 4.3 lection

--- addressbook-web-tests/addressbook-web-tests/AddressBook/Tests/Groups/GroupCreationTests.cs
+++ addressbook-web-tests/addressbook-web-tests/AddressBook/Tests/Groups/GroupCreationTests.cs
@@ -49,7 +49,10 @@ namespace WebAddressbookTests
             app.Groups.CreateGroup(group);
             app.Navigator.GoToGroupsPage();
             List<GroupData> newGroups = app.Groups.GetGroupList();
-            Assert.AreEqual(oldGroups.Count + 1, newGroups.Count);
+            oldGroups.Add(group);
+            oldGroups.Sort();
+            newGroups.Sort();
+            Assert.AreEqual(oldGroups, newGroups);
         }
         #endregion
 
@@ -68,11 +71,35 @@ namespace WebAddressbookTests
             app.Groups.CreateGroup(group);
             app.Navigator.GoToGroupsPage();
             List<GroupData> newGroups = app.Groups.GetGroupList();
-            Assert.AreEqual(oldGroups.Count + 1, newGroups.Count);
+            oldGroups.Add(group);
+            oldGroups.Sort();
+            newGroups.Sort();
+            Assert.AreEqual(oldGroups, newGroups);
         }
         #endregion
 
+        [Test]
 
+        #region Test4
+        public void GroupCreationTest_2()
+        {
+            GroupData group = new GroupData();
+            group.Name = "zGroupName27";
+            group.Header = "zGroupHeader";
+            group.Footer = "zGroupFooter15";
+
+            List<GroupData> oldGroups = app.Groups.GetGroupList();
+            app.Groups.CreateGroup(group);
+            app.Navigator.GoToGroupsPage();
+            List<GroupData> newGroups = app.Groups.GetGroupList();
+            oldGroups.Add(group);
+            oldGroups.Sort();
+            newGroups.Sort();
+            Assert.AreEqual(oldGroups, newGroups);
+
+            //app.Navigator.GoToGPandLogout();
+        }
+        #endregion
 
 
 
--- addressbook-web-tests/addressbook-web-tests/AddressBook/Tests/Groups/GroupModificationTests.cs
+++ addressbook-web-tests/addressbook-web-tests/AddressBook/Tests/Groups/GroupModificationTests.cs
@@ -2,6 +2,7 @@
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
+using System.Collections.Generic;
 using NUnit.Framework;
 
 
@@ -27,10 +28,16 @@ namespace WebAddressbookTests
             group.Header = "zGroupHeader";
             group.Footer = "zGroupFooter15";
 
+            List<GroupData> oldGroups = app.Groups.GetGroupList();
+
             app.Groups.Modify(1, group, newData );
             app.Navigator.GoToGroupsPage();
             //app.Navigator.GoToGPandLogout();
-
+            List<GroupData> newGroups = app.Groups.GetGroupList();
+            oldGroups[0].Name = newData.Name;
+            oldGroups.Sort();
+            newGroups.Sort();
+            Assert.AreEqual(oldGroups, newGroups);
 
 
 
